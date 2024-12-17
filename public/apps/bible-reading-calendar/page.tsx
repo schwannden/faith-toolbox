f6f6668d1f60e50427c02b21433560d043a4bd05
Schwannden Kuo <schwannden.kuo@ui.com>
2024-12-17 13:40:51 +0800

    chore: initial setup

---
+++ public/apps/bible-reading-calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bible Reading Calendar Generator</h1>
+    </div>
+  );
+}
---
+++ src/app/apps/bible-reading-calendar/page.tsx
@@ -0,0 +1,7 @@
+export default function BibleReadingCalendar() {
+  return (
+    <div>
+      <h1>Bible Reading Calendar Generator</h1>
+    </div>
+  );
+}
